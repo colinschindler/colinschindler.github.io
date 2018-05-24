cedeb13d6e6fb418c5efde0cb6341ffacce43f55
Colin Schindler <schindlercolin@gmail.com>
2018-05-24 09:19:39 -0400

    got javascript started for chaning freelancing img and text elements

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,56 @@
-import lozad from 'lozad'
-
 //right click message, include:
 //onMouseDown="popupMsg('your message')"
 //in whatever element you want it to display when rightclicked
 function popupMsg(theMsg) {
 alert(theMsg);
+}
+
+function buttonCycling() {
+  var description = document.getElementById("freelancing-description");
+  var price = document.getElementById("freelancing-price");
+
+  description.textContent = "cycling text!";
+  price.textContent = "invitation"
+
+  document.getElementById('freelancing-image').src="images/cyclists/Frank-Kunkle.jpg";
+}
+
+function buttonPortraits() {
+  var description = document.getElementById("freelancing-description");
+  var price = document.getElementById("freelancing-price");
+
+  description.textContent = "portrait text!";
+  price.textContent = "$75"
+
+  document.getElementById('freelancing-image').src="images/freelancing/portrait.jpg";
+}
+
+function buttonGraduation() {
+  var description = document.getElementById("freelancing-description");
+  var price = document.getElementById("freelancing-price");
+
+  description.textContent = "graduation text!";
+  price.textContent = "$75"
+
+  document.getElementById('freelancing-image').src="images/freelancing/graduation.jpg";
+}
+
+function buttonEngagement() {
+  var description = document.getElementById("freelancing-description");
+  var price = document.getElementById("freelancing-price");
+
+  description.textContent = "Engagement text!";
+  price.textContent = "$125"
+  
+  document.getElementById('freelancing-image').src="images/freelancing/engagement.jpg";
+}
+
+function buttonWedding() {
+  var description = document.getElementById("freelancing-description");
+  var price = document.getElementById("freelancing-price");
+
+  description.textContent = "wedding text!";
+  price.textContent = "$75"
+  
+  document.getElementById('freelancing-image').src="images/freelancing/wedding.jpg";
 }
