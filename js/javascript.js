30db20de1b1d8dd489bd62609998d2a236bde6bd
Colin Schindler <schindlercolin@gmail.com>
2018-06-09 22:09:07 -0400

    added 3 versions of all images and the js to lazyload them all.

--- js/javascript.js
+++ js/javascript.js
@@ -84,3 +84,30 @@ function randCyclistImg() {
 
   document.getElementById("cyclist-img").src = "images/cyclists/" + randCyclist + ".jpg";
 }
+
+// START -- Lazy Loading of Images -- START
+document.addEventListener("DOMContentLoaded", function() {
+  var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+
+  if ("IntersectionObserver" in window) {
+    let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          let lazyImage = entry.target;
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.srcset = lazyImage.dataset.srcset;
+          lazyImage.classList.remove("lazy");
+          lazyImageObserver.unobserve(lazyImage);
+        }
+      });
+    });
+
+    lazyImages.forEach(function(lazyImage) {
+      lazyImageObserver.observe(lazyImage);
+    });
+  } else {
+    // Possibly fall back to a more compatible method here
+  }
+});
+
+// END -- Lazy Loading of Images -- END
